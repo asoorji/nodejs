3425cb352c5b3abd6e3b2b16faf2c595feb127d4
Orji <ndubuisiaso@gmail.com>
2022-11-14 10:33:53 +0100

    Add and delete functionality done

--- tutorial codes/02. modules/app.js
+++ tutorial codes/02. modules/app.js
@@ -5,3 +5,6 @@ console.log(all_stuff.counter(['shaun', 'crystal', 'ryu', 'orji']));
 console.log(all_stuff.adder(4,6));
 
 console.log(all_stuff.adder(all_stuff.pi, 5));
+
+// var counter = require('./count');
+// console.log(counter(['shaun', 'crystal', 'ryu', 'orji']));
